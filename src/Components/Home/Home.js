9599025c8fe9d5f30ccbf955a399411a4818ad48
Vaishnavi25-hub <vaishnavimd2512@gmail.com>
2023-08-21 09:05:42 +0530

    home page design

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,14 +7,36 @@ const HomeCard = () => {
     const navigate = useNavigate();
 
     return (
-        <Card className='text-center'>
-            <Card.Header className='p-3' as='h2'>Bank Home Page</Card.Header>
+        <Card className='text-center' style={{ backgroundColor: 'rgba(0, 0, 255, 0.2)' }}>
+            <Card.Header className='p-3' as='h2'>LCF Co-operative Bank</Card.Header>
             <Card.Body>
                 <Card.Text className='px-2 pb-1'>
-                Hey There, Welcome to our banking system. We are here for you, always!!
+                <h3>Welcome to our Online Banking Portal!</h3>
                 </Card.Text>
                 <Card.Text className='px-2 pb-1'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+<b>Who We Are:</b>
+<br />
+
+Our diverse team combines financial wisdom with technological innovation. We're dedicated to making banking simpler, more accessible, and tailored to your needs.
+<br />
+<b>Our Promise</b>
+<br />
+Security First: Your safety is paramount. Our cutting-edge encryption ensures your data and transactions are always protected.
+<br />
+Intuitive Design: Our user-friendly platform suits tech enthusiasts and newcomers alike, ensuring easy navigation.
+<br />
+Complete Services: From balance checks to bill payments, we offer a comprehensive range of banking solutions.
+<br />
+Anytime Access: Manage your finances 24/7 from anywhere, eliminating time constraints.
+<br />
+Constant Evolution: We embrace technology's potential to improve your banking experience continually.
+<br />
+Dedicated Support: Our customer service team is ready to assist, making sure your journey is smooth.
+<br />
+Join Us Today:
+
+Discover the future of banking with our Online Banking Portal. Experience seamless, secure, and empowered financial management tailored to you. Welcome to the next generation of banking. Welcome to us.
+
                 </Card.Text>
             </Card.Body>
             <Card className='mx-3'>
@@ -44,17 +66,25 @@ const HomeCard = () => {
 
 export const Home = () => {
   return (
-    <Container>
-        <Row className='justify-content-center'>
-            <Col style={{border:'solid 1px green'}} fluid="xs sm" md={{span: 4, order: 2}} className='my-3'>
-                <Container>
-                    <HomeCard/>
-                </Container>
-            </Col>
-            <Col style={{border:'solid 1px red'}} fluid="xs sm" md={{span: 8, order: 1}} className='my-3'>
-
-            </Col>
-        </Row>
+   <Container fluid>
+      <Row className='justify-content-center'>
+        
+        <Col
+          style={{
+            border: 'solid 1px red',
+            backgroundImage: `url('./assets/bg.jpg')`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+          md={8}
+          className='my-3'
+        >
+         
+        </Col>
+        <Col style={{ border: 'solid 1px green', backgroundColor: 'rgba(0, 0, 255, 0.2)'}} md={4} className='my-3'>
+          <HomeCard />
+        </Col>
+      </Row>
     </Container>
   )
 }
